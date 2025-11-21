c1d2db27b2820e6d224818de32e8d470ff96df94
KimZion <kimsadol369785@gmail.com>
2025-11-21 22:30:52 +0900

    LoginView: 일단 화면에 뜰건 뜸.

--- expense-tracker-client/src/main/java/me/goospel/Main.java
+++ expense-tracker-client/src/main/java/me/goospel/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import me.goospel.utils.ViewNavigator;
+import me.goospel.views.LoginView;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -11,9 +13,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(new VBox(), 100, 100);
-        stage.setTitle("Hello World");
-        stage.setScene(scene);
-        stage.show();
+        stage.setTitle("Expense Tracker");
+        ViewNavigator.setMainStage(stage);
+        new LoginView().show();
     }
 }
--- expense-tracker-client/src/main/java/me/goospel/views/LoginView.java
+++ expense-tracker-client/src/main/java/me/goospel/views/LoginView.java
@@ -1,4 +1,30 @@
 package me.goospel.views;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import me.goospel.utils.ViewNavigator;
+
 public class LoginView {
+    private Label expenseTrackerLabel = new Label("Expense Tracker");
+    private TextField usernameField = new TextField();
+    private PasswordField passwordField = new PasswordField();
+    private Button loginButton = new Button("Login");
+    private Label signupLabel = new Label("Don't hava an account? Click here");
+
+    public void show() {
+        Scene scene = createScene();
+        ViewNavigator.switchViews(scene);
+    }
+
+    private Scene createScene() {
+        VBox mainContainerBox = new VBox();
+
+        mainContainerBox.getChildren().addAll(expenseTrackerLabel, usernameField, passwordField, loginButton, signupLabel);
+
+        return new Scene(mainContainerBox, 100, 100);
+    }
 }
