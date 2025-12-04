7637fcca4d1ef7a6782802c5cc04fcd3ee423fe2
KimZion <kimsadol369785@gmail.com>
2025-12-04 21:00:56 +0900

    00:56:20

--- expense-tracker/src/main/java/com/example/expenseTracker/entities/User.java
+++ expense-tracker/src/main/java/com/example/expenseTracker/entities/User.java
@@ -1,4 +1,29 @@
 package com.example.expenseTracker.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "eamil")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 }
--- expense-tracker/src/main/java/com/example/expenseTracker/services/UserService.java
+++ expense-tracker/src/main/java/com/example/expenseTracker/services/UserService.java
@@ -1,4 +1,21 @@
 package com.example.expenseTracker.services;
 
+import com.example.expenseTracker.entities.User;
+import com.example.expenseTracker.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public Optional<User> getUserById(int userId) {
+        log.info("Getting the user by id: {}", userId);
+        return userRepository.findById(userId);
+    }
 }
