6824c2dbf0e7a307a907d930c9094dc226efe4e4
KimZion <kimsadol369785@gmail.com>
2025-12-11 21:38:33 +0900

    01:43:30

--- expense-tracker-client/src/main/java/me/goospel/controllers/LoginController.java
+++ expense-tracker-client/src/main/java/me/goospel/controllers/LoginController.java
@@ -1,10 +1,15 @@
 package me.goospel.controllers;
 
+import com.google.gson.JsonObject;
 import javafx.event.EventHandler;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import me.goospel.utils.ApiUtil;
 import me.goospel.views.LoginView;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
 
 public class LoginController {
     private LoginView loginView;
@@ -22,31 +27,52 @@ public class LoginController {
 
                 String eamil = loginView.getUsernameField().getText();
                 String password = loginView.getPasswordField().getText();
+
+                // authenticate email and password
+                HttpURLConnection connection = null;
+                try {
+                    connection = ApiUtil.fetchApi(
+                            // /api/v1/user/login?email=email@email.com&password=password
+                            "/api/v1/user/login?email=" + eamil + "&password=" + password,
+                            ApiUtil.RequestMethod.POST,
+                            null
+                    );
+
+                    if (connection.getResponseCode() != 200) {
+                        System.out.println(connection.getResponseCode());
+                        System.out.println("Failed to Authenticate");
+                    } else {
+                        System.out.println(connection.getResponseCode());
+                        System.out.println("Login Successful");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
 
-    private boolean validateUser() {
-        TextField usernameField = loginView.getUsernameField();
-        PasswordField passwordField = loginView.getPasswordField();
-
-        if (usernameField.getText().isEmpty() || passwordField.getText().isEmpty() ) {
-            return false;
-        }
-        return true;
-    }
-
-//    private boolean validateUser(){
-//        // empty username
-//        if(loginView.getUsernameField().getText().isEmpty()){
+//    private boolean validateUser() {
+//        TextField usernameField = loginView.getUsernameField();
+//        PasswordField passwordField = loginView.getPasswordField();
+//
+//        if (usernameField.getText().isEmpty() || passwordField.getText().isEmpty() ) {
 //            return false;
 //        }
-//
-//        // empty password
-//        if(loginView.getPasswordField().getText().isEmpty()){
-//            return false;
-//        }
-//
 //        return true;
 //    }
+
+    private boolean validateUser(){
+        // empty username
+        if(loginView.getUsernameField().getText().isEmpty()){
+            return false;
+        }
+
+        // empty password
+        if(loginView.getPasswordField().getText().isEmpty()){
+            return false;
+        }
+
+        return true;
+    }
 }
--- expense-tracker-client/src/main/java/me/goospel/utils/ApiUtil.java
+++ expense-tracker-client/src/main/java/me/goospel/utils/ApiUtil.java
@@ -3,19 +3,39 @@ package me.goospel.utils;
 import com.google.gson.JsonObject;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class ApiUtil {
-    private static final String SPRINGBOOT_URL = "https://localhost:8080";
+    private static final String SPRINGBOOT_URL = "http://localhost:8080";
     public enum RequestMethod{POST, GET, PUT, DELETE}
 
-    public static HttpURLConnection fetchApi(String apiPath, RequestMethod requestMethod, JsonObject jsonObject) {
+    public static HttpURLConnection fetchApi(String apiPath, RequestMethod requestMethod, JsonObject jsonData) {
         try {
             URL url = new URL(SPRINGBOOT_URL + apiPath);
+            System.out.println(url.toString());
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
             connection.setRequestMethod(requestMethod.toString());
+
+            // to send data to api
+            if (jsonData != null && requestMethod != RequestMethod.GET) {
+                connection.setRequestProperty("Content-Type", "application/json, charset=utf-8");
+
+                // expects the response body to be a json type
+                connection.setRequestProperty("Accept", "application/json");
+
+                // allow us to send data to the connected api
+                connection.setDoOutput(true);
+
+                try (OutputStream os = connection.getOutputStream()) {
+                    byte[] input = jsonData.toString().getBytes(StandardCharsets.UTF_8);
+
+                    os.write(input, 0, input.length);
+                }
+            }
 
             return connection;
         } catch (IOException e) {
